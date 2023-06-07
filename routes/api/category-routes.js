7c6154e52bb468220b79c46d74cc158705e3eb0c
DanialDevelops <danialchou101@gmail.com>
2023-06-07 12:12:13 -0400

    added to product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,12 +43,46 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
- 
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      { category_name: req.body.category_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category updated successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
